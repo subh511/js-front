451a56c0ce332a64389965ceffe66da8934f33de
subhranshu sekher dhal <ssdhal98@gmail.com>
2024-01-02 19:23:12 +0530

    intial commit

---
+++ Form/form.js
@@ -0,0 +1,10 @@
+
+const x = document.forms["frm1"];
+
+let text = "";
+
+for(let i=0; i<x.length;i++){
+    text += x.elements[i].value + "<br>";
+}
+
+document.getElementById("demo").innerHTML = text;
