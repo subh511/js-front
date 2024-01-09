7e23f5534189125abbef0637e0fee5ed16f76622
subhranshu sekher dhal <ssdhal98@gmail.com>
2024-01-09 13:12:18 +0530

    phase1

--- GYM-TODO-APP/logic.js
+++ GYM-TODO-APP/logic.js
@@ -1,8 +1,12 @@
+function addTodo() {
+  const title = document.getElementById("title").value;
+  const description = document.getElementById("description").value;
+  // console.log(title.value);
+  // console.log(description.value);
 
-
-function addTodo(){
-    const title = document.getElementById("title");
-    const description = document.getElementById("description");
-    console.log(title.value);
-    console.log(description.value);
+  //creating a child div for container div which is parent 
+  //and appending it as child;
+  const insideDiv = document.createElement("div");
+  insideDiv.innerHTML = title;
+  document.getElementById("container").appendChild(insideDiv);
 }
