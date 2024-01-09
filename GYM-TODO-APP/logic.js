45b28a2d27b5a0ade686a610eec1ce1111b67fcf
subhranshu sekher dhal <ssdhal98@gmail.com>
2024-01-09 11:55:48 +0530

    intial phase

---
+++ GYM-TODO-APP/logic.js
@@ -0,0 +1,8 @@
+
+
+function addTodo(){
+    const title = document.getElementById("title");
+    const description = document.getElementById("description");
+    console.log(title.value);
+    console.log(description.value);
+}
